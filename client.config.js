b5ce5999742e51110692f635281065e24f0224f5
abdlerahman mohamed <abdelrahmanmo147@gmail.com>
2026-03-29 18:05:54 +0200

    template: placeholder config for new clients

--- client.config.js
+++ client.config.js
@@ -1,11 +1,13 @@
 // src/client.config.js
 export const CLIENT = {
-  name: "Santafi",
+  name: "YOUR_RESTAURANT_NAME",
+  appName: "YOUR_RESTAURANT_NAME | مطعمك",
   logo: "/logo.png",
-  primaryColor: "#your-color",
-  cloudinaryCloud: "dkgiwnpfi",
-  cloudinaryPreset: "santafi_products",
-  branches: ["mansoura", "mitghamr", "zagazig"],
+  primaryColor: "#YOUR_COLOR",
+  cloudinaryCloud: "YOUR_CLOUDINARY_CLOUD",
+  cloudinaryPreset: "YOUR_CLOUDINARY_PRESET",
+  branches: ["branch1", "branch2"],
+  ownerBranch: "owner",
   currency: "EGP",
   paymobEnabled: true,
   vodafoneEnabled: true,
